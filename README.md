a2c3f0eeb7853053960e1c8d8dcb46614b5f2d88
Maga Lutfullaev <mhoja9494@gmail.com>
2023-06-10 08:24:53 +0500

    init project

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1 align="center">
+    <b>Dashboard with React and Typescript</b>
+    <br>
+</h1>
+<div align="center">
+
+![version](https://img.shields.io/badge/version-2.0.0-blue.svg)
+![license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+---
+
+### Run npm run start to start the application
---
+++ src/content/dashboards/Crypto/PageHeader.tsx
@@ -0,0 +1,34 @@
+import { Typography, Avatar, Grid } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
+function PageHeader() {
+  const user = {
+    name: 'Catherine Pike',
+    avatar: '/static/images/avatars/1.jpg'
+  };
+  const theme = useTheme();
+
+  return (
+    <Grid container alignItems="center">
+      <Grid item>
+        <Avatar
+          sx={{
+            mr: 2,
+            width: theme.spacing(8),
+            height: theme.spacing(8)
+          }}
+          variant="rounded"
+          alt={user.name}
+          src={user.avatar}
+        />
+      </Grid>
+      <Grid item>
+        <Typography variant="h3" component="h3" gutterBottom>
+          Добрый день, {user.name}!
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default PageHeader;
